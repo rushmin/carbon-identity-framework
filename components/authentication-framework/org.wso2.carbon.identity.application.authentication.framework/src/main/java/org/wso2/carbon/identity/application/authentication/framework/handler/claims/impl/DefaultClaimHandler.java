609548fe47a8ef1605183f191ced264edd253703
Dimuthu De Lanerolle <dimuthud@wso2.com>
2017-05-19 15:37:45 +0530

    Removing unnecessary for loops

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/handler/claims/impl/DefaultClaimHandler.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/handler/claims/impl/DefaultClaimHandler.java
@@ -320,27 +320,21 @@ public class DefaultClaimHandler implements ClaimHandler {
      * @return
      */
     private static String getServiceProviderMappedUserRoles(ApplicationConfig applicationConfig,
-            List<String> locallyMappedUserRoles, String claimSeparator) throws FrameworkException {
+                                                            List<String> locallyMappedUserRoles, String claimSeparator)
+            throws FrameworkException {
 
-            Map<String, String> localToSpRoleMapping = applicationConfig.getRoleMappings();
+        Map<String, String> localToSpRoleMapping = applicationConfig.getRoleMappings();
 
-            if (MapUtils.isEmpty(localToSpRoleMapping)) {
-                return StringUtils.join(locallyMappedUserRoles, ",");
-            }
-
-            StringBuilder spMappedUserRoles = new StringBuilder();
+        if (!MapUtils.isEmpty(localToSpRoleMapping)) {
             for (Map.Entry<String, String> roleMapping : localToSpRoleMapping.entrySet()) {
                 if (locallyMappedUserRoles.contains(roleMapping.getKey())) {
-                    spMappedUserRoles.append(roleMapping.getValue()).append(claimSeparator);
                     locallyMappedUserRoles.remove(roleMapping.getKey());
+                    locallyMappedUserRoles.add(roleMapping.getValue());
                 }
             }
+        }
 
-            for (String remainingRole : locallyMappedUserRoles) {
-                spMappedUserRoles.append(remainingRole).append(claimSeparator);
-            }
-
-        return spMappedUserRoles.toString().substring(0, spMappedUserRoles.length() - 1);
+        return StringUtils.join(locallyMappedUserRoles, claimSeparator);
     }
 
     /**
